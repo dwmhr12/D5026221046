6fcbc5aa56ba5dbfeb2020484ea8a5bd99690af1
dwmhr12 <dewimaharani170104@gmail.com>
2023-12-04 08:36:56 +0700

    Tugas Pertemuan Tanggal 27 Nop

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -1,28 +1,45 @@
 @extends('master2')
-
 @section('konten')
- 
 	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
 	<h3>Data Pegawai</h3>
- 
-	<a href="/pegawai"> Kembali</a>
-	
-	<br/>
-	<br/>
- 
-	<form action="/pegawai/store" method="post">
-		{{ csrf_field() }}
-		<div class = "form-group">
-            <label for = "nama" class = "col-sm-2 control-label"> Name</label>
-            <div class = "col-sm-10">
-               <input name = "nama" type="text " class = "form-control" id = "nama" placeholder = "Masukkan nama ">
-            </div>
-         </div>
-		Nama <input type="text" name="nama" required="required"> <br/>
-		Jabatan <input type="text" name="jabatan" required="required"> <br/>
-		Umur <input type="number" name="umur" required="required"> <br/>
-		Alamat <textarea name="alamat" required="required"></textarea> <br/>
-		<input type="submit" value="Simpan Data">
-	</form>
- 
+
+	<br>
+	<br>
+
+<form action="/pegawai/store" method="post">
+    {{ csrf_field() }}
+    {{-- NAMA --}}
+    <div class = "form-group row">
+        <label for = "nama" class = "col-sm-2 control-label">Nama</label>
+        <div class = "col-sm-8">
+            <input name="nama" type = "text" class = "form-control" id = "nama" placeholder = " Isi Nama Pegawai">
+        </div>
+    </div>
+    {{-- JABATAN --}}
+    <div class = "form-group row">
+        <label for = "jabatan" class = "col-sm-2 control-label">Jabatan</label>
+        <div class = "col-sm-8">
+            <input name="jabatan" type = "text" class = "form-control" id = "jabatan" placeholder = " Isi Jabatan">
+        </div>
+    </div>
+    {{-- UMUR --}}
+    <div class = "form-group row">
+        <label for = "umur" class = "col-sm-2 control-label">Umur</label>
+        <div class = "col-sm-8">
+            <input name="umur" type = "number" class = "form-control" id = "umur" placeholder = "Isi Umur">
+        </div>
+    </div>
+    {{-- ALAMAT --}}
+    <div class = "form-group row">
+        <label for = "alamat" class = "col-sm-2 control-label">Alamat</label>
+        <div class = "col-sm-8">
+            <input name="alamat" type = "text" class = "form-control" id = "alamat" placeholder = "Isi Alamat">
+        </div>
+    </div>
+
+    <div class="col-sm-2 align-items-center mx-auto">
+        <input type="submit" class="btn btn-primary" value="Simpan">
+    </div>
+</form>
+
 @endsection
